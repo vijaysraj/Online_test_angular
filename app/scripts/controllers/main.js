cd36a8a65fd1d041236cb83947532c6c49496d06
Vijay <vijaysraj006@gmail.com>
2016-03-04 16:16:22 +0530

    [initial commit]

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onlineTestAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the onlineTestAngularApp
+ */
+angular.module('onlineTestAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
