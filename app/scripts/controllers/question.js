6752e2c649b03686f04b3a32e39772e8eb45c0e9
Vijay <vijay@Qwinixs-MacBook-Pro.local>
2016-03-23 15:49:20 +0530

    Flow completed

--- app/scripts/controllers/question.js
+++ app/scripts/controllers/question.js
@@ -1,21 +1,62 @@
 'use strict';
 
 angular.module('onlineTestAngularApp')
-.controller('questionCtrl', function ($rootScope, localStorageService, $location, Questionservice) {
+.controller('questionCtrl', function ($rootScope, localStorageService, $location, Questionservice, $scope, $window) {
   var vm = this;
+
+  vm.id = localStorageService.get('id');
 
   vm.auth_token = localStorageService.get('auth-token');
 
-  // if (vm.auth_token === null) {
-  //   if ($location.$$path === '/question') {
-  //     $location.path('/');
-  //   }
-  // }
+  if (vm.auth_token === null) {
+    if ($location.$$path === '/question') {
+      $location.path('/');
+    }
+  }
 
-  Questionservice.getData().then(function(response) {
+  // for restricting page refresh
+
+//   document.onkeydown = function(){
+//   switch (event.keyCode){
+//         case 116 : //F5 button
+//             event.returnValue = false;
+//             event.keyCode = 0;
+//             return false;
+//         case 82 : //R button
+//             if (event.ctrlKey){ 
+//                 event.returnValue = false;
+//                 event.keyCode = 0;
+//                 return false;
+//             }
+//     }
+// }
+
+  Questionservice.getData(2).then(function(response) {
     vm.questionObj = response.data.QuestionList
-    // vm.questionObj = [{"Id":1,"Title":"The difference between the local value and face value of 7 in the numeral 657903 is:","Option_1":"0","Option_2":"7896","Option_3":"6993","Option_4":"903","Answer":"6993"}]
-    console.log(vm.questionObj)
+    // vm.questionObj = fakeResponse
   });
 
+  vm.submitAnswers = function() {
+    var selectedOptions = [];
+    angular.forEach(vm.questionObj, function(question) {
+      selectedOptions.push({ 
+        "QuestionId": question.Id,
+        "Answer": question.selectedOption
+      })
+    });
+
+    var params = {
+      "SectionId": 1,
+      "UserId": vm.id,
+      "Questions": selectedOptions
+    }
+    Questionservice.postData(params).then(function(response){
+      localStorageService.remove('id')
+      localStorageService.remove('auth-token');
+      $location.path('/')
+    })
+    // 1 - Submit the answers
+    // 2 - Get new set of questions
+  }
 });
+
--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -7,19 +7,31 @@ angular.module('onlineTestAngularApp')
 
   vm.auth_token = localStorageService.get('auth-token');
 
-  // if (vm.auth_token != null) {
-  //   if ($location.$$path === '/') {
-  //     $location.path('/question');
-  //   }
-  // }
+  if (vm.auth_token != null) {
+    if ($location.$$path === '/') {
+      $location.path('/question');
+    }
+  }
 
   vm.submit = function () {
-    console.log(vm.registrationForm)
-    Registrationservice.postData(vm.registrationForm).then(function(response) {
+
+   var params = { 
+      "first_name": vm.registrationForm.first_name,
+      "last_name": vm.registrationForm.last_name,
+      "email": vm.registrationForm.email,
+      "password": vm.registrationForm.password,
+      "password_confirmation": vm.registrationForm.password_confirmation,
+      "phone_number": vm.registrationForm.phone_number,
+      "college": vm.registrationForm.college,
+      "year_of_passing": vm.registrationForm.year_of_passing
+    }
+
+    Registrationservice.postData(params).then(function(response) {
       vm.dataObj = response.data;
-      if (vm.dataObj.Status === true) {
-        console
+      if (vm.dataObj.Success === "true") {
         localStorageService.set('auth-token', vm.dataObj.Session.Auth_token);
+        localStorageService.set('id', vm.dataObj.User.Id);
+
         $location.path('/question');
       }
       else {
