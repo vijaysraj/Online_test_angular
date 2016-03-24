0a93e469c99b0730de9cf0b2b5451bf0ae06da2c
Vijay <vijay@Qwinixs-MacBook-Pro.local>
2016-03-24 16:50:18 +0530

    UI changes

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -1,25 +1,39 @@
-$('input[type="submit"]').mousedown(function(){
-  $(this).css('background', '#2ecc71');
+// $('input[type="submit"]').mousedown(function(){
+//   $(this).css('background', '#2ecc71');
+// });
+// $('input[type="submit"]').mouseup(function(){
+//   $(this).css('background', '#1abc9c');
+// });
+
+// $('#loginform').click(function(){
+//   $('.login').fadeToggle('slow');
+//   $(this).toggleClass('green');
+// });
+
+
+
+// $(document).mouseup(function (e)
+// {
+//     var container = $(".login");
+
+//     if (!container.is(e.target) // if the target of the click isn't the container...
+//         && container.has(e.target).length === 0) // ... nor a descendant of the container
+//     {
+//         container.hide();
+//         $('#loginform').removeClass('green');
+//     }
+// });
+
+
+
+
+$(document).ready(function(){
+  $('.toggle').on('click', function() {
+  $('.container').stop().addClass('active');
 });
-$('input[type="submit"]').mouseup(function(){
-  $(this).css('background', '#1abc9c');
+
+$('.close').on('click', function() {
+  $('.container').stop().removeClass('active');
+});
 });
 
-$('#loginform').click(function(){
-  $('.login').fadeToggle('slow');
-  $(this).toggleClass('green');
-});
-
-
-
-$(document).mouseup(function (e)
-{
-    var container = $(".login");
-
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        container.hide();
-        $('#loginform').removeClass('green');
-    }
-});
